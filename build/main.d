d0832d59f7eed0e4881bfa776dc8b24163a93da9
KellinSchaap <200557@student.glu.nl>
2023-02-05 11:49:59 +0100

    Setup for development made

--- build/main.d
+++ build/main.d
@@ -1,78 +1,78 @@
 main.o: \
- D:/Hobby/ProjectGames/ZelfGemaakt/20222023/GBAProjects/ONGOING/ProjIsland/source/main.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\main_include.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\gba.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\display.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\gba.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\key_inputs.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\interrupt.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\system.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_attributes.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\tileblock_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\palette_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\bitfield.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_attributes.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\background_headers\bg_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\background_headers\bg_attributes.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\physics.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\mathematics.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\physics.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\physics.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\sprite.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\tileblock_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\palette_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\sprite.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\sprite.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\main.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\types.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\emulator.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\emulator.cpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\emulator.hpp \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\assets\player\player.h \
- d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\assets\player\player.c
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\main_include.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\gba.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\display.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\gba.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\key_inputs.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\interrupt.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\system.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_attributes.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\tileblock_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\palette_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\bitfield.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_attributes.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.cpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\background_headers\bg_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\background_headers\bg_attributes.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\physics.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\mathematics.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\physics.cpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\physics.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\sprite.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\tileblock_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\palette_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\sprite.cpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\sprite.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\main.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\object_headers\object_defines.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\types.cpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\types.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\emulator.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\source\bps\emulator.cpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\include\bps\emulator.hpp:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\assets\player\player.h:
-d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projisland\assets\player\player.c:
+ D:/Hobby/ProjectGames/ZelfGemaakt/20222023/GBAProjects/ONGOING/ProjectIslandGBA/source/main.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\main_include.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\gba.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\display.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\gba.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\key_inputs.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\interrupt.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\system.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_attributes.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\tileblock_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\palette_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\bitfield.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_attributes.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\background_headers\bg_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\background_headers\bg_attributes.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\physics.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\mathematics.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\physics.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\physics.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\sprite.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\tileblock_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\palette_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\sprite.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\sprite.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\main.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\types.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\emulator.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\emulator.cpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\emulator.hpp \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\assets\player\player.h \
+ d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\assets\player\player.c
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\main_include.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\gba.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\display.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\gba.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\key_inputs.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\interrupt.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\system.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_attributes.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\tileblock_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\palette_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\bitfield.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_attributes.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.cpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\background_headers\bg_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\background_headers\bg_attributes.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\physics.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\mathematics.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\physics.cpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\physics.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\sprite.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\tileblock_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\palette_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\sprite.cpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\sprite.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\main.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\object_headers\object_defines.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\types.cpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\types.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\emulator.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\source\bps\emulator.cpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\include\bps\emulator.hpp:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\assets\player\player.h:
+d:\hobby\projectgames\zelfgemaakt\20222023\gbaprojects\ongoing\projectislandgba\assets\player\player.c:
